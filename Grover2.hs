3008774bd184c3695d37c73ba35f67091e96c43f
psycjw <psycjw@nottingham.ac.uk>
2019-03-14 19:41:36 +0000

    Finished implementations of Grover's algorithm
    Two different implementations of Grover's algorithm, where each can be run on custom boolean functions.

--- Grover2.hs
+++ Grover2.hs
@@ -1,22 +1,49 @@
-import Data.Monoid
-import System.Time
 import Text.Printf
-
+import Data.Monoid
 import QIO.Qio
 import QIO.QioSyn
 import QIO.QioClass
 import QIO.QExamples
+import System.Time
 
+{-
+Example
+-1 -3 -4
+2 3 -4
+1 -2 4
+-1 2 -3
+
+9 Solutions
+
+1 2 3 4
+-------
+0 0 0 0.
+0 0 1 0.
+0 0 1 1.
+0 1 0 1.
+0 1 1 1.
+1 0 0 0.
+1 1 0 0.
+1 1 0 1.
+1 1 1 0.
+-}
+
+{-
+The main method is used to input a custom boolean problem, taking in the amount of variables,
+the boolean predicates and the amount of solutions to the problem (optional).
+Grover's algorithm is then performed using an oracle created with the predicates inputted,
+and solution(s) found are printed at the end. This is timed using the System.Time module.
+-}
 main :: IO ()
 main = do
   putStrLn "How many variables?"
   inV <- getLine
   let n = (read inV :: Int)
-  putStrLn "Input each predicate with lines of AND operations, where each line will be OR'd, with an empty line after the last."
-  putStrLn "For example, (x1 & -x2) | x1 | x2 would be inputted as\n1 -2\n1\n2\n\n-----"
+  putStrLn "Input each predicate in its dimacs version of the clause normal form syntax on its own line, with an empty line after the last."
+  putStrLn "For example, (-x1 V x2) & x1 & -x2 would be inputted as\n-1 2\n1\n-2\n\n-----"
   inP <- getLines
   let ps =  filter (not . null) $ map (removeOOB n) inP
-  putStrLn "How many solutions does this formula have? This is used to calculate how many Grover iterations are needed to get an accurate answer."
+  putStrLn "How many solutions does this formula have? Input ? if unknown. This is used to calculate how many Grover iterations are needed to get an accurate answer."
   inS <- getLine
   let s = if inS == "?" then -1 else (read inS :: Int)
   start <- getClockTime
@@ -39,6 +66,14 @@ the number of variables, n, and the list of the predicates, ps.
 If s is 1, then Grover's algorithm needs to be run once, with the amount of iterations needed
 equalling (pi/4)(N^1/2) where N = 2^n.
 
+If s is -1, then there could be any amount of solutions to the problem, and so it is
+impossible to know how many iterations are necessary. Therefore, we must iterate for
+increasing values of s, i.e.
+(pi/4)(N^1/2), (pi/4)(N/2)^1/2, (pi/4)(N/4)^1/2, (pi/4)(N/8)^1/2....
+From this, we can see the maximum number of iterations necessary is
+(pi/4)(N^1/2)(1 + 2^(-1/2) + 4^(-1/2) + 8^(-1/2) + ...) = (pi/4)(N^1/2)(2 + 2^1/2) iterations.
+If no solutions are found after this, then there are no solutions to the problem.
+
 For any s > 1, then Grover's algorithm needs to be run to get as many solutions as desired
 by the user, and each time the amount of iterations needed is (pi/4)(N/s)^1/2.
 -}
@@ -46,7 +81,12 @@ getSolution :: Int -> Int -> [[Int]] -> IO ()
 getSolution s n ps =
   if s == 1 then do
     out <- run $ customGrover (calcIterations (2^n) 1) n ps
-    print out
+    if head out then
+      print $ tail out
+    else
+      getSolution 1 n ps
+  else if s == -1 then
+    getUnknownSolution 1 n ps
   else do
     printf "How many solutions do you want out of %d.\n" s
     inK <- getLine
@@ -56,12 +96,26 @@ getSolution s n ps =
     else
       getSolutions s [] n ps k
 
+getUnknownSolution :: Int -> Int -> [[Int]] -> IO ()
+getUnknownSolution s n ps =
+  if (i >= round ((pi * sqrt(2^(fromIntegral n))/4) * (2 + sqrt(2)))) then do
+    putStrLn "No solutions found."
+  else do
+    printf "Performing Grover's algorithm with %d iterations.\n" i
+    out <- run $ customGrover i n ps
+    if head out then
+      print $ tail out
+    else
+      getUnknownSolution (s+1) n ps
+  where
+    i = calcIterations (2^n) (2^s)
+
 getSolutions :: Int -> [[Bool]] -> Int -> [[Int]] -> Int -> IO ()
 getSolutions s bs n ps k =
   if (k > 0) then do
     out <- run $ customGrover (calcIterations (2^n) s) n ps
-    if out `notElem` bs then
-      getSolutions s (bs ++ [out]) n ps (k-1)
+    if head out && (tail out) `notElem` bs then
+      getSolutions s (bs ++ [tail out]) n ps (k-1)
     else
       getSolutions s bs n ps k
   else
@@ -69,20 +123,32 @@ getSolutions s bs n ps k =
 
 customGrover :: Int -> Int -> [[Int]] -> QIO [Bool]
 customGrover i n ps = do
-  qI <- mkQbits n False
-  qO <- mkQbits (length ps) True
-  applyU (uhadN qI <> uhadN qO)
-  applyU (groverSteps i ps qI qO)
-  measQbits qI
+  qI <- mkQbits n True
+  applyU (uhadN qI)
+  qO <- mkQbits (length ps) False
+  qZ <- mkQbit False
+  applyU (groverSteps i ps qI qO qZ)
+  measQbits ([qZ] ++ qI)
 
-groverSteps :: Int -> [[Int]] -> [Qbit] -> [Qbit] -> U
-groverSteps i ps qI qO =
-  foldr (<>) mempty (replicate i (grover ps qI qO))
+groverSteps :: Int -> [[Int]] -> [Qbit] -> [Qbit] -> Qbit -> U
+groverSteps i ps qI qO qZ =
+  foldr (<>) mempty (replicate i (grover ps qI qO qZ))
+  <> halfGrover ps qI qO qZ <>
+  uZZ qZ
 
-grover :: [[Int]] -> [Qbit] -> [Qbit] -> U
-grover ps qI qO =
-  customOracle 0 ps qI qO <>
+grover :: [[Int]] -> [Qbit] -> [Qbit] -> Qbit -> U
+grover ps qI qO qZ =
+  halfGrover ps qI qO qZ <>
+  uZZ qZ <>
+  urev (halfGrover ps qI qO qZ) <>
   diffuse qI
+
+halfGrover :: [[Int]] -> [Qbit] -> [Qbit] -> Qbit -> U
+halfGrover ps qI qO qZ =
+  customOracle 0 ps qI qO <>
+  unotN qO <>
+  condXn (qO ++ [qZ]) <>
+  unotN qO
 
 customOracle :: Int -> [[Int]] -> [Qbit] -> [Qbit] -> U
 customOracle _ [] _ _ = mempty
@@ -92,7 +158,7 @@ customOracle n (p:ps) qI qO =
   unotN (unots p qI) <>
   customOracle (n+1) ps qI qO
   where
-    unots p qI = map (\x -> qI!!((abs x)-1)) $ filter (< 0) p
+    unots p qI = map (\x -> qI!!(x-1)) $ filter (> 0) p
     conds p qI = map (\x -> qI!!((abs x)-1)) p
 
 calcIterations :: Int -> Int -> Int
