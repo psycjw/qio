17b16f14e6405c947f5f3a0a90c55ee9d4f4ee56
psycjw <psycjw@nottingham.ac.uk>
2019-04-03 18:39:52 +0100

    Final Grover.hs file

--- Grover.hs
+++ Grover.hs
@@ -1,28 +1,48 @@
-import Data.Monoid
-import System.Time
-import Text.Printf
-
 import QIO.Qio
 import QIO.QioSyn
-import QIO.QioClass
-import QIO.QExamples
 
+import System.TimeIt
+
+{-
+Executes Grover's algorithm over a custom inputted oracle function, with any amount of solutions, whether known or unknown.
+
+After receiving the amount of variables in the function, and the function inputted in the described form, the program will
+create a list of "predicates", where each predicate consists of a list of integers in the corresponding line of the function,
+where NOT-ted variables are negative.
+
+Next, the amount of solutions is asked for, and results in 3 cases.
+1. It is known that there is 1 solution to the function.
+2. It is known that there is more than 1 solution to the function.
+3. The amount of solutions to the function is not known.
+
+Case 1. 1 solution
+Run Grover's algorithm with "iterations n 1" iterations.
+
+Case 2. >1 solutions, s
+Run Grover's algorithm with "iterations n s" iterations, repeating until the desired amount of solutions, k where 0 < k <= s, is found.
+
+Case 3. Unknown amount of solutions.
+Run Grover's algorithm with increasing amounts of iterations, equal to "iterations n 1", "iterations n 2", "iterations n 4",
+"iterations n 8",... up to (pi/4)(N^1/2)(2 + 2^1/2) iterations.
+-}
 main :: IO ()
 main = do
   putStrLn "How many variables?"
   inV <- getLine
-  let n = (read inV :: Int)
+  let n = read inV :: Int
   putStrLn "Input each predicate with lines of AND operations, where each line will be OR'd, with an empty line after the last."
   putStrLn "For example, (x1 & -x2) | x1 | x2 would be inputted as\n1 -2\n1\n2\n\n-----"
   inP <- getLines
-  let ps =  filter (not . null) $ map (removeOOB n) inP
-  putStrLn "How many solutions does this formula have? This is used to calculate how many Grover iterations are needed to get an accurate answer."
+  let ps =  filter (not . null) $ map (filter (\p -> abs p <= n || p /= 0)) inP
+  putStrLn "How many solutions does this formula have? This is used to calculate how many Grover iterations are needed. Input '?' if the amount of solutions is not known."
   inS <- getLine
-  let s = if inS == "?" then -1 else (read inS :: Int)
-  start <- getClockTime
-  getSolution s n ps
-  stop <- getClockTime
-  putStrLn ("The total time taken was " ++ (timeDiffToString (diffClockTimes stop start)))
+  if inS == "?"
+    then timeIt $ getUnknownSolution n 0 ps
+    else do
+      let s = read inS :: Int
+      putStrLn $ "\nProbability of finding a solution after " ++ (show $ iterations n s) ++ " iteration(s) is: "
+      probability n (iterations n s) ps
+      timeIt $ solve n s ps
   where
     getLines = do
       l <- getLine
@@ -32,92 +52,131 @@ main = do
           xs <- getLines
           return (x:xs)
 
-{-
-getSolution's arguments are the number of solutions, s (-1 in the case of an unknown amount),
-the number of variables, n, and the list of the predicates, ps.
+solve :: Int -> Int -> [[Int]] -> IO ()
+solve n s ps =
+  if s == 1
+    then do
+      putStrLn "Search time:"
+      out <- timeIt $ run $ grover n (iterations n 1) ps
+      if head out
+        then do
+          putStrLn "Found solution:"
+          putStrLn $ show $ tail out
+          putStrLn "\nTotal time:"
+        else do
+          putStrLn "Found non-solution:"
+          putStrLn $ show $ tail out
+          solve n 1 ps
+    else do
+      putStrLn $ "\nHow many solutions do you want out of " ++ (show s) ++ "?"
+      inK <- getLine
+      let k = read inK :: Int
+      if (k < 1 || k > s)
+        then getSolutions n s ps [] s
+        else getSolutions n s ps [] k
 
-If s is 1, then Grover's algorithm needs to be run once, with the amount of iterations needed
-equalling (pi/4)(N^1/2) where N = 2^n.
+getSolutions :: Int -> Int -> [[Int]] -> [[Bool]] -> Int -> IO ()
+getSolutions n s ps bs k = do
+  if (k > 0)
+    then do
+      putStrLn "\nSearch time:"
+      out <- timeIt $ run $ grover n (iterations n s) ps
+      if head out
+        then if (tail out) `notElem` bs
+            then do
+              putStrLn "Found new solution:"
+              putStrLn $ show $ tail out
+              getSolutions n s ps (bs ++ [tail out]) (k-1)
+            else do
+              putStrLn "Found old solution:"
+              putStrLn $ show $ tail out
+              getSolutions n s ps bs k
+        else do
+          putStrLn "Found non-solution:"
+          putStrLn $ show $ tail out
+          getSolutions n s ps bs k
+    else do
+      putStrLn "\nSolution(s):"
+      mapM_ print bs
+      putStrLn "\nTotal time:"
 
-For any s > 1, then Grover's algorithm needs to be run to get as many solutions as desired
-by the user, and each time the amount of iterations needed is (pi/4)(N/s)^1/2.
--}
-getSolution :: Int -> Int -> [[Int]] -> IO ()
-getSolution s n ps =
-  if s == 1 then do
-    out <- run $ customGrover (calcIterations (2^n) 1) n ps
-    print out
-  else do
-    printf "How many solutions do you want out of %d.\n" s
-    inK <- getLine
-    let k = (read inK :: Int)
-    if (k < 0 || k > s) then
-      getSolutions s [] n ps s
-    else
-      getSolutions s [] n ps k
+getUnknownSolution :: Int -> Int -> [[Int]] -> IO ()
+getUnknownSolution n s ps = do
+  if s >= n || i >= round ((pi * sqrt(2^(fromIntegral n))/4) * (2 + sqrt(2)))
+    then putStrLn "No solutions found."
+    else do
+      putStrLn "\nSearch time:"
+      out <- timeIt $ run $ grover n i ps
+      if head out
+        then do
+          putStrLn "Found solution:"
+          putStrLn $ show $ tail out
+          putStrLn "\nTotal time:"
+        else do
+          putStrLn "Found non-solution:"
+          putStrLn $ show $ tail out
+          getUnknownSolution n (s+1) ps
+  where
+    i = iterations n (2^s)
 
-getSolutions :: Int -> [[Bool]] -> Int -> [[Int]] -> Int -> IO ()
-getSolutions s bs n ps k =
-  if (k > 0) then do
-    out <- run $ customGrover (calcIterations (2^n) s) n ps
-    if out `notElem` bs then
-      getSolutions s (bs ++ [out]) n ps (k-1)
-    else
-      getSolutions s bs n ps k
-  else
-    mapM_ print bs
+iterations :: Int -> Int -> Int
+iterations n s = round $ (pi/4) * sqrt((fromIntegral (2^n))/(fromIntegral s))
 
-customGrover :: Int -> Int -> [[Int]] -> QIO [Bool]
-customGrover i n ps = do
-  qI <- mkQbits n False
-  qO <- mkQbits (length ps) True
-  applyU (uhadN qI <> uhadN qO)
-  applyU (groverSteps i ps qI qO)
-  measQbits qI
+grover :: Int -> Int -> [[Int]] -> QIO [Bool]
+grover n i ps = do
+  (qI, qA) <- initialize n ((length ps)+1)
+  let groverStep = oracle ps qI <> diffuse qI
+  applyU (foldr (<>) mempty (replicate i groverStep))
+  applyU (testForSolution 0 ps qI qA)
+  measQbits ([last qA] ++ qI)
 
-groverSteps :: Int -> [[Int]] -> [Qbit] -> [Qbit] -> U
-groverSteps i ps qI qO =
-  foldr (<>) mempty (replicate i (grover ps qI qO))
+probability :: Int -> Int -> [[Int]] -> IO ()
+probability n i ps = putStrLn $ show $ sim $ probOfSolution
+  where
+    probOfSolution = do
+      (qI, qA) <- initialize n ((length ps)+1)
+      let groverStep = oracle ps qI <> diffuse qI
+      applyU (foldr (<>) mempty (replicate i groverStep))
+      applyU (testForSolution 0 ps qI qA)
+      measQbit $ last qA
 
-grover :: [[Int]] -> [Qbit] -> [Qbit] -> U
-grover ps qI qO =
-  customOracle 0 ps qI qO <>
-  diffuse qI
-
-customOracle :: Int -> [[Int]] -> [Qbit] -> [Qbit] -> U
-customOracle _ [] _ _ = mempty
-customOracle n (p:ps) qI qO =
-  unotN (unots p qI) <>
-  condXn (conds p qI ++ [qO!!n]) <>
-  unotN (unots p qI) <>
-  customOracle (n+1) ps qI qO
+oracle :: [[Int]] -> [Qbit] -> U
+oracle [] _      = mempty
+oracle (p:ps) qI =
+  unitaryN unot (unots p qI) <>
+  condN (\q -> uphase q pi) (conds p qI) <>
+  unitaryN unot (unots p qI) <>
+  oracle ps qI
   where
     unots p qI = map (\x -> qI!!((abs x)-1)) $ filter (< 0) p
     conds p qI = map (\x -> qI!!((abs x)-1)) p
 
-calcIterations :: Int -> Int -> Int
-calcIterations n s = round $ (pi/4) * sqrt((fromIntegral n)/(fromIntegral s))
+testForSolution :: Int -> [[Int]] -> [Qbit] -> [Qbit] -> U
+testForSolution _ [] _ qA      =
+  unitaryN unot qA <>
+  condN unot qA <>
+  unitaryN unot (init qA)
+testForSolution c (p:ps) qI qA =
+  unitaryN unot (unots p qI) <>
+  condN unot ((conds p qI) ++ [qA!!c]) <>
+  unitaryN unot (unots p qI) <>
+  testForSolution (c+1) ps qI qA
+  where
+    unots p qI = map (\x -> qI!!((abs x)-1)) $ filter (< 0) p
+    conds p qI = map (\x -> qI!!((abs x)-1)) p
 
-removeOOB :: Int -> [Int] -> [Int]
-removeOOB _ [] = []
-removeOOB n ps =
-  if (any (\p -> abs p > n || p == 0) ps)
-    then []
-    else ps
+initialize :: Int -> Int -> QIO ([Qbit], [Qbit])
+initialize i a = do
+  qI <- mkQbits i True
+  qA <- mkQbits a False
+  applyU (unitaryN uhad qI)
+  return (qI, qA)
 
-{-
-When diffuse is called, a solution state stored in qI will have a
-negative phase, while non-solution states will not. Diffuse increases the
-amplitude of negative-phased states via an inversion around the mean amplitude,
-effectively "picking" out solution states.
--}
 diffuse :: [Qbit] -> U
 diffuse qI =
-  uhadN qI <>
-  unotN qI <>
-  condZn qI <>
-  unotN qI <>
-  uhadN qI
+  unitaryN uhad (init qI) <>
+  condN unot qI <>
+  unitaryN uhad (init qI)
 
 mkQbits :: Int -> Bool -> QIO [Qbit]
 mkQbits n b = mkQbits' n b []
@@ -135,21 +194,10 @@ measQbits qs = measQbits' qs []
       b <- measQbit q
       measQbits' qs (bs ++ [b])
 
--- performs a c^nNot gate over n qubits, storing the result in the last qubit given
-condXn :: [Qbit] -> U
-condXn (ql:[]) = unot ql
-condXn (q:qs) = cond q (\x -> if x then (condXn qs) else mempty)
+unitaryN :: (Qbit -> U) -> [Qbit] -> U
+unitaryN _ []      = mempty
+unitaryN uf (q:qs) = uf q <> unitaryN uf qs
 
-condZn :: [Qbit] -> U
-condZn (ql:[]) = uphase ql pi
-condZn (q:qs) = cond q (\x -> if x then (condZn qs) else mempty)
-
--- performs multiple unot gates over each qubit input
-unotN :: [Qbit] -> U
-unotN [] = mempty
-unotN (q:qs) = unot q <> unotN qs
-
--- performs multiple hadamard gates over each qubit input
-uhadN :: [Qbit] -> U
-uhadN [] = mempty
-uhadN (q:qs) = uhad q <> uhadN qs
+condN :: (Qbit -> U) -> [Qbit] -> U
+condN uf (q:[]) = uf q
+condN uf (q:qs) = cond q (\x -> if x then (condN uf qs) else mempty)
